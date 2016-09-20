8f6a07c3c30ebf5d07616dfd1b652fe526da397b
Mr Dung <dung13890@gmail.com>
2016-09-20 07:53:38 +0000

    fix ok

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -18,9 +18,11 @@
         <meta property="og:image:height" content="300">
 
         {{ HTML::style('vendor/bootstrap/css/bootstrap.min.css') }}
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
         {{ HTML::style('template/css/common.css') }}
         {{ HTML::style('template/css/responsive/common.css') }}
