5c5a7d810a9598867ed93fcc26a0014db896066c
Mr Dung <dung13890@gmail.com>
2016-09-26 02:46:02 +0000

    fix css ok

--- resources/views/frontend/_partials/top.blade.php
+++ resources/views/frontend/_partials/top.blade.php
@@ -1,7 +1,7 @@
 <div id="top">
     <div class="language pull-right">
         {{ trans('repositories.lang') }}
-        <strong ><a @if (session('locale') === 'vi') class="active" @endif href="{{ route('home.locale', 'vi') }}" title="Tiếng Việt">VN</a></strong> |
+        <strong style="margin-left: 10px;" ><a @if (session('locale') === 'vi') class="active" @endif href="{{ route('home.locale', 'vi') }}" title="Tiếng Việt">VN</a></strong> |
         <a @if (session('locale') === 'en') class="active" @endif href="{{ route('home.locale', 'en') }}" title="Tiếng Anh">EN</a>
         <a href="{{ $configs['facebook'] }}" title="Facebook" style="margin-left: 10px">
             {!! HTML::image('assets/img/facebook_icon.png', 'ICON-FB') !!}
