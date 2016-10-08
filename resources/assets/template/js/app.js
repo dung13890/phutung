b05b1beb3103158fd62a308288a7c22d9661bd5a
xuanquynh <nguyenxuanquynh2210vghy@gmail.com>
2016-10-08 11:02:02 +0700

    RecursiveSubmenu

--- resources/assets/template/js/app.js
+++ resources/assets/template/js/app.js
@@ -1,40 +1,23 @@
+function openSidenav() {
+    $('#menu .links').css('left', 0);
+}
+
+function closeSidenav() {
+    $('#menu .links').css('left', '-300px');
+}
+
 $(document).ready(function(){
-    // toggle mobile menu
-    $('#menu .toggle-menu').click(function(){
-        var display = $(this).attr('display');
-        if (display == 0) {
-            $(this).attr('display', 1);
-            $('#menu .links > ul').css('height', $(window).height());
-            $('#menu .links').animate({'left': '-15%'}, 450, function(){
-                $('body').css({
-                    'overflow-y': 'hidden'
-                });
-            });
-        } else {
-            $(this).attr('display', 0);
-            $('#menu .links').animate({'left': '-100%'}, 450, function(){
-                $('body').css({
-                    'overflow-y': 'scroll'
-                });
-            });
-
-            $('#menu .links > ul').css('height', 0);
-        }
-    });
-
     $('.show-search').click(function(){
         $(this).css('display', 'none');
-        $('#menu .form').css({
-            'width': '120px',
-            'right': '115px',
-            'display': 'block'
-        });
+        $('#menu .form').css({width: '120px',right: '115px',display: 'block'});
         $('#menu .form input[type="text"]').focus();
     });
 
-    $('#menu .form input[type="text"]').focusout(function(){
-        $('#menu .form').css('display', 'none');
-        $('.show-search').css('display', 'block');
+    var keyword = $('#menu .form input[type="text"]').attr('placeholder');
+    $('#menu .form input[type="text"]').focus(function() {
+        $(this).attr('placeholder', '');
+    }).focusout(function() {
+        $(this).attr('placeholder', keyword);
     });
 
     $('#headerLinks').html($('#header .links').html());
