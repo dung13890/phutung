97b546847fcfcd9dd632a80c63838aef8be3230c
Mr Dung <dung13890@gmail.com>
2016-10-17 10:13:49 +0000

    fix ok

--- resources/views/frontend/product/show.blade.php
+++ resources/views/frontend/product/show.blade.php
@@ -133,7 +133,7 @@
                        <table>
                            <tr>
                                <td><div class="fb-like" data-href="{{ Request::url() }}" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div></td>
-                               <td><div class="g-plusone" data-annotation="none" data-height="14" data-href="{{ Request::url() }}"></div></td>
+                               <td style="padding-top: 9px;"><div class="g-plusone" data-annotation="none" data-height="14" data-href="{{ Request::url() }}"></div></td>
                            </tr>
                        </table>
                     </div>
@@ -188,7 +188,7 @@
                    <table>
                        <tr>
                            <td><div class="fb-like" data-href="{{ Request::url() }}" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div></td>
-                           <td>
+                           <td style="padding-top: 9px;">
                                <div class="g-plusone" data-annotation="none" data-height="14" data-href="{{ Request::url() }}"></div>
                            </td>
                        </tr>
