2c40ecfd5e0ecd197a7e9ea3ad7b03cd66a59131
Mr Dung <dung13890@gmail.com>
2016-11-01 02:14:09 +0000

    fix ok

--- resources/views/frontend/post/show.blade.php
+++ resources/views/frontend/post/show.blade.php
@@ -47,7 +47,34 @@
     	{!! $item->description !!}
 
         <br>
+
+        <div class="like-share">
+           <table>
+               <tr>
+                   <td><div class="fb-like" data-href="{{ Request::url() }}" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div></td>
+                   <td style="padding-top: 9px;"><div class="g-plusone" data-annotation="none" data-height="14" data-href="{{ Request::url() }}"></div></td>
+               </tr>
+           </table>
+        </div>
     </div>
 </div>
 
 @endsection
+
+@push('prescripts')
+
+<script src="https://apis.google.com/js/platform.js" async defer>
+  {lang: 'vi'}
+</script>
+
+<div id="fb-root"></div>
+
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.7";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+@endpush
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     	<title>{!! $heading or $configs['name'] !!}</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="description" content="{!! $description or $configs['description'] !!}">
         <meta name="keywords" content="{!! $keywords or $configs['keywords'] !!}">
         <meta property="og:url" content="{!! Request::url() !!}" />
