c106fda29e3c13592567c2da7855ecd1ff756da6
Mr Dung <dung13890@gmail.com>
2016-11-03 07:36:59 +0000

    add favicon

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -5,7 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     	<title>{!! $heading or $configs['name'] !!}</title>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
         <meta name="description" content="{!! $description or $configs['description'] !!}">
         <meta name="keywords" content="{!! $keywords or $configs['keywords'] !!}">
         <meta property="og:url" content="{!! Request::url() !!}" />
